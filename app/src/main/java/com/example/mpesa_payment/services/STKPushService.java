966b3c43678f6b58b1a3d51148f0a055b9912f74
robynroby <robinsonmwaura254@gmail.com>
2023-03-31 19:36:16 +0300

    implimentation of daraja2.0 api to an android app

---
+++ app/src/main/java/com/example/mpesa_payment/services/STKPushService.java
@@ -0,0 +1,19 @@
+package com.example.mpesa_payment.services;
+
+
+
+import com.example.mpesa_payment.model.STKPush;
+import com.example.mpesa_payment.model.AccessToken;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface STKPushService {
+    @POST("mpesa/stkpush/v1/processrequest")
+    Call<STKPush> sendPush(@Body STKPush stkPush);
+
+    @GET("oauth/v1/generate?grant_type=client_credentials")
+    Call<AccessToken> getAccessToken();
+}
